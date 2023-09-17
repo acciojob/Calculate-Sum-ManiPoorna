35c2aa9f705fb3ecd9db92b1370f2d8b9b88af5e
Mani Poorna <118202451+ManiPoorna@users.noreply.github.com>
2023-09-17 09:28:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required.
 document.getElementById("sum_btn").addEventListener('click',()=>{
 	let sum = +document.getElementById("n1").value+(+document.getElementById("n2").value)
-	document.getElementById("sum").innerText = sum;
+	document.getElementById("Sum").innerText = sum;
 })
